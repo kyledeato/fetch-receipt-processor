a035f74dae7e2f0a9da8066ac1f7164c36834e66
Kyle Deato <kyledeato@gmail.com>
2025-04-16 21:52:32 -0700

    clean up comments fix

--- index.js
+++ index.js
@@ -11,13 +11,10 @@ app.use(express.json());
 // MEMORY
 const receipts = {};
 
-// POST /receipts/process
+// ===== POST /receipts/process =====
 // receives receipt JSON, stores with unique ID, and returns ID
 app.post('/receipts/process', (req, res) => {
   const receipt = req.body;
-
-  // add validation HERE
-
 
   const id = uuidv4(); // generate unique ID for receipt
   receipts[id] = receipt; //store receipt in memory
@@ -27,7 +24,7 @@ app.post('/receipts/process', (req, res) => {
   res.json({ id }); // respond with new ID
 });
 
-// GET /receipts/:id/points
+// ===== GET /receipts/:id/points =====
 // looks up receipt by ID and returns points
 app.get('/receipts/:id/points', (req, res) => {
   const id = req.params.id;
